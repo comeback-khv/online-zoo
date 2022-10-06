1d3945c723a87186d8c94fc390f573aed914bb0e
Anushavan Anatolian <zhigg@mail.ru>
2022-10-06 15:49:41 +1000

    feat: add active range

--- online-zoo/src/donate.js
+++ online-zoo/src/donate.js
@@ -4,14 +4,17 @@ import './index.js';
 const donateOption = document.querySelectorAll('.donate__amount-range-input');
 const donatePrice = document.querySelectorAll('.donate__price')
 const donateInput = document.querySelector('.donate__amount-input');
+const donateLabel = document.querySelectorAll('.donate__amount-range-label')
 
 function setOption() {
     for (let i = 0; i < donateOption.length; i++) {
         donateOption[i].addEventListener('click', () => {
             for (let i = 0; i < donateOption.length; i++) {
                 donatePrice[i].classList.remove('donate__price--active');
+                donateLabel[i].classList.remove('donate__amount-range-label--active');
             }
             donatePrice[i].classList.add('donate__price--active');
+            donateLabel[i].classList.add('donate__amount-range-label--active');
             donateInput.value = donateOption[i].id;
         })
     }
@@ -20,13 +23,15 @@ setOption()
 
 donateInput.oninput = function () {
     this.value = this.value.substr(0, 4);
-    let inputValue= this.value;
+    let inputValue = this.value;
     for (let i = 0; i < donateOption.length; i++) {
         if (inputValue == donateOption[i].id) {
             for (let i = 0; i < donateOption.length; i++) {
                 donatePrice[i].classList.remove('donate__price--active');
+                donateLabel[i].classList.remove('donate__amount-range-label--active');
             }
             donatePrice[i].classList.add('donate__price--active');
+            donateLabel[i].classList.add('donate__amount-range-label--active');
         }
     }
 }
