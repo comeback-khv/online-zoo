98dd4c94445dbb8ec496ab6db9e6916a6973f5e8
Anushavan Anatolian <zhigg@mail.ru>
2022-10-06 13:08:57 +1000

    feat: add burger menu

--- online-zoo/src/donate.js
+++ online-zoo/src/donate.js
@@ -1,17 +1,34 @@
 import './sass/main-page2.scss';
+import './index.js';
 
-const donateOption = document.querySelectorAll('.donate__amount-range-container');
+const donateOption = document.querySelectorAll('.donate__amount-range-input');
 const donatePrice = document.querySelectorAll('.donate__price')
-const donateRangeContainer = document.querySelector('.donate__amount-range');
+const donateInput = document.querySelector('.donate__amount-input');
 
-for (let i = 0; i < donateOption.length; i++) {
-    donateOption[i].addEventListener('click', () => {
-        for (let i = 0; i < donateOption.length; i++) {
-            donatePrice[i].classList.remove('donate__price--active');
-        }
-        donatePrice[i].classList.add('donate__price--active');
-    })
-  //  if (!donatePrice[i].classList.contains('donate__price--active')) {
-  //      donatePrice[5].classList.add('donate__price--active')
-  //  }
+function setOption() {
+    for (let i = 0; i < donateOption.length; i++) {
+        donateOption[i].addEventListener('click', () => {
+            for (let i = 0; i < donateOption.length; i++) {
+                donatePrice[i].classList.remove('donate__price--active');
+            }
+            donatePrice[i].classList.add('donate__price--active');
+            donateInput.value = donateOption[i].id;
+        })
+    }
 }
+setOption()
+
+donateInput.oninput = function () {
+    this.value = this.value.substr(0, 4);
+    let inputValue= this.value;
+    for (let i = 0; i < donateOption.length; i++) {
+        if (inputValue == donateOption[i].id) {
+            for (let i = 0; i < donateOption.length; i++) {
+                donatePrice[i].classList.remove('donate__price--active');
+            }
+            donatePrice[i].classList.add('donate__price--active');
+        }
+    }
+}
+
+
--- online-zoo/src/index.js
+++ online-zoo/src/index.js
@@ -1,2 +1,34 @@
 // Импорт
 import './sass/main.scss';
+
+const headerNavList = document.querySelector('.header__nav-list');
+const headerTitle = document.querySelector('.header__title');
+const headerCopyright = document.querySelector('.header-copyright');
+const burger = document.querySelector('.burger');
+const xIcon = document.querySelector('.x-icon');
+const body = document.querySelector('.body');
+const bodyActive = document.querySelector('.body--active');
+
+function openMenu() {
+    headerNavList.classList.add('header__nav-list--active');
+    headerTitle.classList.add('header__title--active');
+    headerCopyright.classList.add('header-copyright--active');
+    xIcon.classList.add('x-icon--active');
+    body.classList.add('body--active');
+}
+
+function closeMenu() {
+    headerNavList.classList.remove('header__nav-list--active');
+    headerTitle.classList.remove('header__title--active');
+    headerCopyright.classList.remove('header-copyright--active');
+    xIcon.classList.remove('x-icon--active');
+    body.classList.remove('body--active');
+}
+burger.addEventListener('click', openMenu);
+xIcon.addEventListener('click', closeMenu);
+document.addEventListener('click', (event => {
+    if (event.target.classList.contains('body--active')) {
+        closeMenu();
+    }
+}))
+
